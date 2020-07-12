77188194e115c71434d6a8f9162e019aac3d4a3e
abhishekraj11303372 <43966764+abhishekraj11303372@users.noreply.github.com>
2020-07-13 01:19:44 +0530

    launch settings port , swagger and course add

--- CleanArch/CleanArch.Api/Startup.cs
+++ CleanArch/CleanArch.Api/Startup.cs
@@ -2,14 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.IoC;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace CleanArch.Api
 {
@@ -25,8 +31,24 @@ namespace CleanArch.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<UniversityDBContext>(options =>
+            {
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("UniversityDBConnection"));
+            });
+
             services.AddControllers();
+
+            services.AddSwaggerGen(c => 
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "University Api", Version = "v1"});
+            });
+
+            services.AddMediatR(typeof(Startup));
+
+            RegisterServices(services);
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -38,6 +60,12 @@ namespace CleanArch.Api
 
             app.UseHttpsRedirection();
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c=> 
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "University Api v1");
+            });
+
             app.UseRouting();
 
             app.UseAuthorization();
@@ -46,6 +74,11 @@ namespace CleanArch.Api
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
